53f254e9646f3b892e027a36ed3353a308837500
jyedev04 <jyedev04@gmail.com>
2025-03-13 17:40:09 +0900

    refac: JPQL 기반 findByIdWithUser를 QueryDSL로 변경

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,5 +1,8 @@
 package org.example.expert.domain.todo.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -7,16 +10,21 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.entity.QTodo;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.QUser;
 import org.example.expert.domain.user.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +32,7 @@ public class TodoService {
 
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
+    private final JPAQueryFactory jpaQueryFactory;
 
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
@@ -49,18 +58,53 @@ public class TodoService {
 
     public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDate startDate, LocalDate endDate) {
         Pageable pageable = PageRequest.of(page - 1, size);
+        QTodo todo = QTodo.todo;
+        QUser user = QUser.user;
 
-        Page<Todo> todos = todoRepository.findByTodos(weather, startDate, endDate, pageable);
+        BooleanBuilder builder = new BooleanBuilder();
 
-        return todos.map(todo -> new TodoResponse(
-                todo.getId(),
-                todo.getTitle(),
-                todo.getContents(),
-                todo.getWeather(),
-                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
-                todo.getCreatedAt(),
-                todo.getModifiedAt()
+        if(weather != null) {
+            builder.and(todo.weather.eq(weather));
+        }
+
+        if(startDate != null) {
+            builder.and(todo.modifiedAt.goe(startDate.atStartOfDay()));
+        }
+
+        if(endDate != null) {
+            builder.and(todo.modifiedAt.loe(endDate.atTime(23,59,59)));
+        }
+
+        // 데이터 조회
+        JPAQuery<Todo> query = jpaQueryFactory.selectFrom(todo)
+                .leftJoin(todo.user, user).fetchJoin()
+                .where(builder)
+                .orderBy(todo.modifiedAt.desc());
+
+        // 페이징 처리
+        List<Todo> content = query
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 전체 데이터 개수
+        long totalSize = Optional.ofNullable(jpaQueryFactory
+                .select(todo.count())
+                .from(todo)
+                .where(builder)
+                .fetchOne()
+        ).orElse(0L);
+
+        return new PageImpl<>(content, pageable, totalSize).map(todoItem -> new TodoResponse(
+                todoItem.getId(),
+                todoItem.getTitle(),
+                todoItem.getContents(),
+                todoItem.getWeather(),
+                new UserResponse(todoItem.getUser().getId(), todoItem.getUser().getEmail()),
+                todoItem.getCreatedAt(),
+                todoItem.getModifiedAt()
         ));
+
     }
 
     public TodoResponse getTodo(long todoId) {
