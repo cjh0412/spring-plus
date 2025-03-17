428681f77456d227ebe11d90820fc8743c9e3e19
cjh0412 <jyedev04@gmail.com>
2025-03-17 19:27:42 +0900

    refact: Spring Security를 도입  - 기존 filter, argument resolverfmf spring security 로 수정

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -2,13 +2,14 @@ package org.example.expert.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.expert.config.JwtUtil;
-import org.example.expert.config.PasswordEncoder;
+
 import org.example.expert.domain.auth.dto.request.SigninRequest;
 import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.exception.AuthException;
 import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.example.expert.domain.user.repository.UserRepository;
--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -1,18 +1,42 @@
 package org.example.expert.domain.common.dto;
 
-import lombok.Getter;
-import org.example.expert.domain.user.enums.UserRole;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Slf4j
 @Getter
 public class AuthUser {
 
     private final Long id;
     private final String email;
-    private final UserRole userRole;
+    private final Collection<? extends GrantedAuthority> authorities;
 
-    public AuthUser(Long id, String email, UserRole userRole) {
+
+    public AuthUser(Long id, String email, UserRole role) {
         this.id = id;
         this.email = email;
-        this.userRole = userRole;
+        this.authorities = List.of(new SimpleGrantedAuthority(role.name()));
     }
+
+    public UserRole getUserRole() {
+        log.info("AuthUser에서 getUserRole 호출 - authorities: {}", authorities);
+        if (authorities == null || authorities.isEmpty()) {
+            log.error("AuthUser에 저장된 권한이 없음");
+            throw new IllegalArgumentException("권한을 찾을 수 없습니다.");
+        }
+
+        for (GrantedAuthority authority : authorities) {
+            log.info("AuthUser에서 반환하는 권한: {}", authority.getAuthority());
+            return UserRole.valueOf(authority.getAuthority());
+        }
+        throw new IllegalArgumentException("권한을 찾을 수 없습니다.");
+    }
+
 }
